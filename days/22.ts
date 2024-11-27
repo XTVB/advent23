6f77187d1dc55975abd40be567d4675af4883af1
Abdusalam Dihan <abdu.dihan@gmail.com>
2024-11-27 14:21:37 +0000

    day 22 solution optimised, processing time from ~3200ms -> ~230ms

--- days/22.ts
+++ days/22.ts
@@ -3,7 +3,7 @@ const day = path.basename(import.meta.file, '.ts')
 console.log(`Day ${day}`)
 const input = await Bun.file(`./inputs/${day}.txt`).text()
 
-const inputT = `1,0,1~1,2,1
+const inputA = `1,0,1~1,2,1
 0,0,2~2,0,2
 0,2,3~2,2,3
 0,0,4~0,2,4
@@ -38,12 +38,14 @@ const blocks = input.split('\n').map((blockStr, index) => {
   const [start, end] = blockStr.split('~').map((point) => point.split(',').map((p) => parseInt(p)) as Point3D)
   // names starting with 'A'
   const block: Block = [String.fromCharCode(65 + index), start, end]
+  // with the full array of blocks numbers are clearer than characters
+  // const block: Block = [`${index}`, start, end]
   return block
 })
 const maxX = Math.max(...blocks.map((block) => Math.max(block[1][0], block[2][0]))) + 1
 const maxY = Math.max(...blocks.map((block) => Math.max(block[1][1], block[2][1]))) + 1
 const maxZ = Math.max(...blocks.map((block) => Math.max(block[1][2], block[2][2]))) + 1
-const startingVol = initVolume(maxX, maxY, maxZ)
+let startingVol = initVolume(maxX, maxY, maxZ)
 
 const volModBlock = (block: Block, disintegrate: boolean = false, volume: Volume = startingVol): Volume => {
   const [name, start, end] = block
@@ -52,7 +54,7 @@ const volModBlock = (block: Block, disintegrate: boolean = false, volume: Volume
   for (let i = x1; i <= x2; i++) {
     for (let j = y1; j <= y2; j++) {
       for (let k = z1; k <= z2; k++) {
-        if (disintegrate && volume[i][j][k] === name) {
+        if (disintegrate) {
           volume[i][j][k] = '.'
         } else {
           volume[i][j][k] = name
@@ -60,6 +62,38 @@ const volModBlock = (block: Block, disintegrate: boolean = false, volume: Volume
       }
     }
   }
+  return volume
+}
+
+// slightly more efficient than volModBlock twice
+const volMoveBlock = (source: Block, target: Block, volume: Volume = startingVol): Volume => {
+  const [sourceName, sourceStart, sourceEnd] = source
+  const [_, targetStart, targetEnd] = target
+  const [sourceX1, sourceY1, sourceZ1] = sourceStart
+  const [sourceX2, sourceY2, sourceZ2] = sourceEnd
+  const targetZ1 = targetStart[2]
+  const targetZ2 = targetEnd[2]
+
+  if (sourceZ1 !== sourceZ2) {
+    // save a little bit of editing by calculating the overlap
+    const outside = Math.min(sourceZ2 - targetZ2, sourceZ2 - sourceZ1 + 1)
+    for (let i = sourceX1; i <= sourceX2; i++) {
+      for (let j = sourceY1; j <= sourceY2; j++) {
+        for (let k = 0; k < outside; k++) {
+          volume[i][j][sourceZ2 - k] = '.'
+          volume[i][j][targetZ1 + k] = sourceName
+        }
+      }
+    }
+  } else {
+    for (let i = sourceX1; i <= sourceX2; i++) {
+      for (let j = sourceY1; j <= sourceY2; j++) {
+        volume[i][j][sourceZ1] = '.'
+        volume[i][j][targetZ1] = sourceName
+      }
+    }
+  }
+
   return volume
 }
 
@@ -113,15 +147,12 @@ const printYz = (volume: Volume = startingVol): void => {
 
 const sortBlocks = (blocks: Block[]): Block[] => {
   return blocks.sort((a, b) => {
-    const [_, startA, endA] = a
-    const [__, startB, endB] = b
-    const lowestA = Math.min(startA[2], endA[2])
-    const lowestB = Math.min(startB[2], endB[2])
-    return lowestA - lowestB
+    return a[1][2] - b[1][2]
   })
 }
 
-const supports = new Map<string, string[]>()
+const supports = new Map<string, Set<string>>()
+const supportedBy = new Map<string, Set<string>>()
 
 const canFall = (block: Block, volume: Volume = startingVol, updateSupports: boolean = true): boolean => {
   const [name, start, end] = block
@@ -137,7 +168,8 @@ const canFall = (block: Block, volume: Volume = startingVol, updateSupports: boo
         }
         if (spot === '-' || spot !== name) {
           if (updateSupports && spot !== '-') {
-            supports.set(spot, [...(supports.get(spot) || []), name])
+            supports.set(spot, (supports.get(spot) || new Set()).add(name))
+            supportedBy.set(name, (supportedBy.get(name) || new Set()).add(spot))
           }
           hitSomething = true
         }
@@ -151,18 +183,19 @@ const fallBlock = (block: Block, volume: Volume = startingVol): Block => {
   const [name, start, end] = block
   const [x1, y1, z1] = start
   const [x2, y2, z2] = end
-  if (canFall(block, volume)) {
-    volModBlock(block, true, volume)
-    const newBlock: Block = [name, [x1, y1, z1 - 1], [x2, y2, z2 - 1]]
-    volModBlock(newBlock, false, volume)
-    return fallBlock(newBlock, volume)
+  let newBlock: Block = [name, [x1, y1, z1], [x2, y2, z2]]
+  while (canFall(newBlock, volume)) {
+    newBlock = [name, [x1, y1, newBlock[1][2] - 1], [x2, y2, newBlock[2][2] - 1]]
   }
-  return block
+  if (block[1][2] !== newBlock[1][2]) {
+    volMoveBlock(block, newBlock, volume)
+  }
+  return newBlock
 }
 
 // if allowed is true, it will return true if the block is disintegratable
 // first part wants true, second part wants false
-const filterBlocks = (block: Block, fallenBlocks: Block[], allowed: boolean = true, debug: boolean = false) => {
+const filterBlocks = (block: Block, allowed: boolean = true, debug: boolean = false) => {
   const [name] = block
   const supported = supports.get(name)
   if (!supported) {
@@ -171,28 +204,19 @@ const filterBlocks = (block: Block, fallenBlocks: Block[], allowed: boolean = tr
   if (debug) {
     console.log(supported)
   }
-  const ifDisintegrated = volModBlock(block, true, structuredClone(startingVol))
-  const wouldFall = supported.filter((name) => {
-    const block = fallenBlocks.find((block) => block[0] === name)!
-    const can = canFall(block, ifDisintegrated, false)
-    if (debug) {
-      console.log(`Block ${name} can fall: ${can}`)
+  for (const supportedName of supported) {
+    if (supportedBy.get(supportedName)?.size === 1) {
+      return !allowed
     }
-    return can
-  })
-  if (debug) {
-    console.log(wouldFall)
   }
-  return wouldFall.length === 0 ? allowed : !allowed
+  return allowed
 }
 
 sortBlocks(blocks)
 blocks.map((block) => volModBlock(block))
 const fallenBlocks = blocks.map((block) => fallBlock(block))
-// const disintegratableBlocks = fallenBlocks.filter((block) => filterBlocks(block, fallenBlocks)).map((block) => block[0])
-const nonDisintegratableBlocks = fallenBlocks
-  .filter((block) => filterBlocks(block, fallenBlocks, false))
-  .map((block) => block[0])
+// const disintegratableBlocks = fallenBlocks.filter((block) => filterBlocks(block)).map((block) => block[0])
+const nonDisintegratableBlocks = fallenBlocks.filter((block) => filterBlocks(block, false)).map((block) => block[0])
 
 // console.log(blocks)
 // console.log(fallenBlocks)
@@ -200,27 +224,22 @@ const nonDisintegratableBlocks = fallenBlocks
 // printYz()
 // console.log(supports)
 // console.log(disintegratableBlocks)
+// console.log(nonDisintegratableBlocks)
 
 // easier to keep the filter the same and do some maths so as to not have to filter twice
 console.log(`Answer Part A: ${fallenBlocks.length - nonDisintegratableBlocks.length}`)
 
-const fallBlockOnce = (block: Block, volume: Volume = startingVol): number => {
-  const [name, start, end] = block
-  const [x1, y1, z1] = start
-  const [x2, y2, z2] = end
-  if (canFall(block, volume)) {
-    volModBlock(block, true, volume)
-    const newBlock: Block = [name, [x1, y1, z1 - 1], [x2, y2, z2 - 1]]
-    volModBlock(newBlock, false, volume)
-    return 1
-  }
-  return 0
-}
+const countFalls = (block: Block, blocks: Block[] = fallenBlocks): number => {
+  const [name] = block
+  const fallen = [name]
 
-const countFalls = (block: Block, blocks: Block[] = fallenBlocks, volume: Volume = startingVol): number => {
-  const ifDisintegrated = volModBlock(block, true, structuredClone(startingVol))
-  return blocks.slice(blocks.indexOf(block)).reduce((count, block) => {
-    return count + fallBlockOnce(block, ifDisintegrated)
+  return blocks.slice(blocks.indexOf(block) + 1).reduce((count, block) => {
+    const supportedByBlock = [...(supportedBy.get(block[0])?.values() ?? [])]
+    if (supportedByBlock.length > 0 && supportedByBlock.every((name) => fallen.includes(name))) {
+      fallen.push(block[0])
+      return count + 1
+    }
+    return count
   }, 0)
 }
 
